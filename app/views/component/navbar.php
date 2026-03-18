5c16f27920d9ecd16e40dc48cf2546b0cf640df4
clarence008-web <clarence.008@ski.sch.id>
2026-03-18 19:41:25 +0700

    Progress fix navbar

--- app/views/component/navbar.php
+++ app/views/component/navbar.php
@@ -1,83 +1,69 @@
-<link rel="stylesheet" href="../css/navbar.css">
-
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-<script defer src="../js/navbar.js"></script>
-
-
-<nav class="tc-navbar">
-
-    <!-- LOGO -->
-    <div class="tc-logo">
-        Trade<span>Coin</span>
-    </div>
-
-
-    <!-- MENU -->
-    <ul class="tc-menu">
-        <li><a href="#">Search Item & Service</a></li>
-        <li><a href="#">Sell Item & Service</a></li>
-        <li><a href="#">Guide</a></li>
-        <li><a href="#">Contact Us</a></li>
-    </ul>
-
-
-    <!-- RIGHT SIDE -->
-    <div class="tc-right">
-
-        <!-- CART -->
-        <div class="tc-cart">
-            <i class="fas fa-shopping-cart"></i>
-            <span class="tc-cart-count">2</span>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            Trade<span>Coin</span>
         </div>
-
-
-        <!-- USER MENU -->
-        <div class="tc-user">
-
-            <!-- USER ICON -->
-            <div class="tc-avatar" id="tcUserIcon">
-                <i class="fas fa-user-circle"></i>
+    
+        <ul class="menu">
+            <li><a href="/products">Search Item & Service</a></li>
+            <li><a href="#">Sell Item & Service</a></li>
+            <li><a href="#">Guide</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    
+        <div class="right">
+            <div class="cart">
+            <a href="/cart">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="cart-count">5</span>
+            </a>
             </div>
-
-            <!-- DROPDOWN -->
-            <div class="tc-dropdown" id="tcDropdown">
-
-                <p class="tc-balance">
-                    Points : <b>500</b>
+    
+       
+            <div class="user">
+                <div class="avatar" id="tcUserIcon">
+                    <i class="fas fa-user-circle"></i>
+                </div>
+            </div>
+    
+            <div class="dropdown" id="tcDropdown">
+    
+                <p class="balance">
+                        Points : <b>500</b>
                 </p>
-
+    
                 <a href="#">
                     <i class="fa fa-user"></i>
-                    Account Profile
+                        Account Profile
                 </a>
-
+    
                 <a href="#">
                     <i class="fa fa-gear"></i>
-                    Account Setting
-                </a>
-
+                        Account Setting
+                    </a>
+    
                 <a href="#">
                     <i class="fa fa-clock"></i>
-                    Transaction History
-                </a>
-
+                        Transaction History
+                    </a>
+    
                 <a href="#">
                     <i class="fa fa-right-from-bracket"></i>
-                    Logout
-                </a>
-
+                        Logout
+                    </a>
+    
+                </div>
             </div>
-
-        </div>
-
-
-        <!-- MOBILE HAMBURGER -->
-        <div class="tc-hamburger" id="tcHamburger">
-            <i class="fas fa-bars"></i>
-        </div>
-
-    </div>
-
-</nav>
+    
+    </nav>
+    
+    <script defer src="../js/navbar.js"></script>
+</body>
+</html>
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,6 @@
 const userIcon = document.getElementById("tcUserIcon");
 const dropdown = document.getElementById("tcDropdown");
 
-const hamburger = document.getElementById("tcHamburger");
-const menu = document.querySelector(".tc-menu");
-
-
 userIcon.addEventListener("click",(e)=>{
 
 e.stopPropagation();
